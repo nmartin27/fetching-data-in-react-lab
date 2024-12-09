243123f4366a3da0344cc56ffc92a7c31fed8007
nmartin <nate.martin27@gmail.com>
2024-12-09 11:52:10 -0500

    starships dipslayed in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 // src/App.jsx
 
+import StarshipService from "./services/starshipService.jsx";
+
+
 const App = () => {
 
   return (
-    <h1>Hello world!</h1>
+    <>
+    <h1>Star Wars API</h1>
+    <h2>Search</h2>
+    <form >
+      <label htmlFor="">Search Term: </label>
+      <input type="text" />
+      <input type="submit" value="Search"/>
+    </form>
+    
+    <StarshipService />
+    </>
   );
 }
 
